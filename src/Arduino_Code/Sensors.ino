b1fa15dcb2ef8e421b2df9a2805bc579a3272192
James Omoya <james@james.(none)>
2013-05-01 23:36:46 +0200

    l

---
+++ src/Arduino_Code/Sensors.ino
@@ -0,0 +1,236 @@
+#include <stdint.h>
+#include <DHT.h>
+#include "Wire.h"
+#include "EggBus.h"
+
+EggBus eggBus;
+
+#define DHTPIN 17 //analog pin 3
+#define DHTTYPE DHT22  
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println(F("Air Quality Egg - EPA Serial Build v1.0"));
+  Serial.println(F("======================================================================\r\n"));
+  Serial.println(F("Timestamp, Sensor Type, Sensor Value, Sensor Units, Sensor Resistance"));
+  Serial.println(F("----------------------------------------------------------------------"));  
+}
+
+void loop(){
+  uint8_t   egg_bus_address;
+  float i_scaler = 0.0;
+  uint32_t r0 = 0;
+  uint32_t measured_value = 0;
+  
+  eggBus.init();
+    
+  while((egg_bus_address = eggBus.next())){
+    uint8_t numSensors = eggBus.getNumSensors();
+    for(uint8_t ii = 0; ii < numSensors; ii++){
+      Serial.print(millis(), DEC);
+      Serial.print(F(", "));      
+      
+      Serial.print(eggBus.getSensorType(ii));
+      Serial.print(F(", "));
+      
+      i_scaler = eggBus.getIndependentScaler(ii);      
+      measured_value = eggBus.getSensorIndependentVariableMeasure(ii);
+      r0 = eggBus.getSensorR0(ii);
+      
+      //if(isnan(eggBus.getSensorValue(ii))){Serial.print("0.000000");          
+//}
+      /*else{Serial.print(eggBus.getSensorValue(ii), 8);}*/
+      Serial.print(eggBus.getSensorValue(ii), DEC);
+      Serial.print(F(", "));    
+        //  Serial.print(eggBus.getSensorType(ii));
+  
+      //Serial.print(F(", "));      
+
+      Serial.print(eggBus.getSensorUnits(ii));            
+      Serial.print(F(", "));    
+      
+      if(measured_value == 0xffffffff){
+        Serial.print(F("OPEN CIRCUIT"));
+      }
+      else{
+        Serial.print(measured_value * i_scaler * r0, 8);
+      }   
+      
+      Serial.println();
+    }
+  }
+
+  Serial.print(millis(), DEC);
+  Serial.print(F(", "));      
+  
+  Serial.print("Humidity");
+  Serial.print(F(", "));
+  float currHumidity = getHumidity();
+  Serial.print(currHumidity, 8);
+  Serial.print(F(", "));      
+
+  Serial.println(F("%, n/a"));
+  delay(2500);
+
+
+
+  Serial.print(millis(), DEC);
+  Serial.print(F(", "));      
+  
+  Serial.print("Temperature");
+  Serial.print(F(", "));
+  float currentTemperature = getTemperature();
+  Serial.print(currentTemperature, 8);
+  Serial.print(F(", "));      
+
+  Serial.println(F("degrees C, n/a"));
+  delay(2500);
+
+  Serial.println();
+}
+
+void printAddress(uint8_t * address){
+  for(uint8_t jj = 0; jj < 6; jj++){
+    if(address[jj] < 16) Serial.print("0");
+    Serial.print(address[jj], HEX);
+    if(jj != 5 ) Serial.print(":");
+  }
+  Serial.println();
+}
+
+//--------- DHT22 humidity ---------
+float getHumidity(){
+  float h = dht.readHumidity();
+  if (isnan(h)){
+    //failed to get reading from DHT    
+    delay(2500);
+    h = dht.readHumidity();
+    if(isnan(h)){
+      return -1; 
+    }
+  } 
+  else return h;
+}
+
+//--------- DHT22 temperature ---------
+float getTemperature(){
+  float t = dht.readTemperature();
+  if (isnan(t)){
+    //failed to get reading from DHT
+    delay(2500);
+    t = dht.readTemperature();
+    if(isnan(t)){
+      return -1; 
+    }
+  } 
+  return t;
+}
+
+
+
+/*#include <stdint.h>
+
+#include "Wire.h"
+#include "EggBus.h"
+
+EggBus eggBus;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  uint8_t   egg_bus_address;
+  float x_scaler = 0.0;
+  float y_scaler = 0.0;
+  float i_scaler = 0.0;
+  uint32_t measured_value = 0;
+  uint32_t r0 = 0;
+  
+  eggBus.init();
+  while((egg_bus_address = eggBus.next())){
+    Serial.println("===========================");
+    Serial.print("Egg Bus Address: 0x");
+    Serial.println(egg_bus_address, HEX);
+
+    Serial.print("Firmware Version: ");
+    Serial.println(eggBus.getFirmwareVersion(), DEC);
+
+    Serial.print("  Sensor Address: ");
+    printAddress(eggBus.getSensorAddress());             
+    
+    uint8_t numSensors = eggBus.getNumSensors();
+    for(uint8_t ii = 0; ii < numSensors; ii++){
+    
+      Serial.println("---------------------------");
+      Serial.print("  Sensor Index: ");
+      Serial.println(ii, DEC);       
+      
+      Serial.print("    Sensor Type: ");
+      Serial.println(eggBus.getSensorType(ii));
+     
+      Serial.print("   Indep. Scaler: ");
+      i_scaler = eggBus.getIndependentScaler(ii);
+      Serial.println(i_scaler, 8);
+      
+      Serial.print("  Table X Scaler: ");
+      x_scaler = eggBus.getTableXScaler(ii);
+      Serial.println(x_scaler, 8);     
+     
+      Serial.print("  Table Y Scaler: ");
+      y_scaler = eggBus.getTableYScaler(ii);
+      Serial.println(y_scaler, 8);         
+
+      Serial.print(" Measured Value: ");
+      measured_value = eggBus.getSensorIndependentVariableMeasure(ii);
+      Serial.print(measured_value, DEC);        
+      Serial.print(" => ");
+      Serial.println(measured_value * i_scaler, 8);
+
+      Serial.print("              R0: ");
+      r0 = eggBus.getSensorR0(ii);
+      Serial.println(r0);
+      
+      Serial.print("Implied Resistance: ");
+      if(measured_value == 0xffffffff){
+        Serial.println("OPEN CIRCUIT");
+      }
+      else{
+        Serial.println(measured_value * i_scaler * r0, 8);
+      }
+      
+      uint8_t xval, yval, row = 0;
+      while(eggBus.getTableRow(ii, row++, &xval, &yval)){
+        Serial.print("     Table Row ");
+        Serial.print(row);
+        Serial.print(": [");
+        Serial.print(xval, DEC);
+        Serial.print(", ");        
+        Serial.print(yval, DEC);        
+        Serial.print("] => [");
+        Serial.print(x_scaler * xval, 8);
+        Serial.print(", ");
+        Serial.print(y_scaler * yval, 8);
+        Serial.println("]");
+      }
+      
+      Serial.print("    Sensor Value: ");
+      Serial.println(eggBus.getSensorValue(ii), DEC);    
+      
+      Serial.print("    Sensor Units: ");
+      Serial.println(eggBus.getSensorUnits(ii));            
+    }
+  }
+  
+  delay(5000);
+}
+
+void printAddress(uint8_t * address){
+  for(uint8_t jj = 0; jj < 6; jj++){
+    if(address[jj] < 16) Serial.print("0");
+    Serial.print(address[jj], HEX);
+    if(jj != 5 ) Serial.print(":");
+  }
+  Serial.println();
+}*/
---
+++ src/Processing Code/sql.pde
@@ -0,0 +1,145 @@
+import processing.serial.*;
+import de.bezier.data.sql.*;
+
+Serial myPort;
+String sensorReading="";
+PFont font;
+String[] list;
+Double Temperature;
+Double Humidity;
+Double CO;
+Double NO2;
+int count = 0;
+int id = 1;
+float LAT = 57.7077;
+float LNG = 11.9718;
+int lat = 0;
+MySQL msql;
+
+
+void setup() {
+  size(1000,500);
+  myPort = new Serial(this, "COM9", 9600);
+  myPort.bufferUntil('\n');
+  font = createFont(PFont.list()[2],32);
+  textFont(font);
+   String user     = "test";
+    String pass     = "test";    //
+    String database = "testing";
+    
+    msql = new MySQL( this, "46.239.124.29:3306", database, user, pass);
+
+    if ( msql.connect() )
+    {
+        println("Connection established");
+        msql.query( "SELECT COUNT(*) FROM sensor_data_new" );
+        msql.next();
+        println( "number of rows: " + msql.getInt(1));
+    }
+    else
+    {
+       // connection failed !;
+    }
+}
+
+void draw() {
+  //The serialEvent controls the display
+}  
+
+void serialEvent (Serial myPort){
+ sensorReading = myPort.readStringUntil('\n');
+  if(sensorReading != null){
+    sensorReading=trim(sensorReading);
+     list = split(sensorReading, ',');
+  }
+
+  //writeText("Sensor Reading: " + sensorReading);
+   if (sensorReading.contains("Temperature")) {
+              Temperature = Double.parseDouble(list[2]);
+               // writeText("Sensor Reading: " + Temperature);
+               println(Temperature);
+               count++;
+          }
+    if (sensorReading.contains("Humidity")) {
+              Humidity = Double.parseDouble(list[2]);
+             //writeText("Sensor Reading: " + Humidity);
+               println(Humidity);
+              count++;
+          }
+     if (sensorReading.contains("CO")) {
+           //float ff = Float.parseFloat(list[2]);
+              CO = Double.parseDouble(list[2]);
+              //if((Double.parseDouble(CO)/1) != Double.parseDouble(CO)){
+              //if(CO.equals("nan")){
+                //println("Found NAN");
+              //}
+             // writeText("Sensor Reading: " + CO);
+               println(CO);
+              count++;
+          }
+          
+       if (sensorReading.contains("NO2")) {
+              NO2 = Double.parseDouble(list[2]);
+              //writeText("Sensor Reading: " + NO2);
+               println(NO2);
+            count++;
+          }
+            if(lat == 1){
+          LAT = 57.7077;
+          LNG = 11.9718;
+          }
+          if(lat == 2){
+          LAT = 57.708733;
+          LNG = 11.938019;
+          }
+          if(lat == 3){
+          LAT = 57.689102;
+          LNG = 11.975098;
+          }
+          if(lat == 4){
+          LAT = 57.712768;
+          LNG = 11.89167;
+          }
+          if(lat == 5){
+          LAT = 57.680293;
+          LNG = 11.954498;
+          }
+          if(lat == 6){
+          LAT = 57.736234;
+          LNG = 12.019043;
+          }
+          if(lat == 7){
+          LAT = 57.698644;
+          LNG = 11.928406;
+          }
+          if(lat == 8){
+          LAT = 57.713134;
+          LNG = 12.033119;
+          }
+          if(lat == 9){
+          LAT = 57.691121;
+          LNG = 12.004967;
+          }
+          if(lat == 10){
+          LAT = 57.71075;
+          LNG = 12.00943;
+          lat = 1;
+          }
+        
+          if(count == 4){
+         msql.execute( "INSERT INTO sensor_data_new (LAT, LNG, TEMP, HUMIDITY , CO, NO2) VALUES ("/*+id+","*/+LAT+"," + LNG +","+Temperature+"," + Humidity +","+CO+"," +NO2+")");
+           println("Inserted");
+           delay(3000);
+           id++;
+           lat++;
+           count = 0;  
+        }
+
+}
+
+
+void writeText(String textToWrite){
+  background(255);
+  fill(0);
+  text(textToWrite, 1, 100);   
+}
